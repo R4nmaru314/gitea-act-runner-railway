44ed2bc20a71ceac463fbfe1c4f14351973723c4
r4nmaru <n.lehmann@r4nmaru.de>
2025-11-05 13:04:16 +0100

    prepare runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+
 cd /data
 if [ ! -f .runner ]; then
   echo "No .runner—registering with flags..."
   if ! /usr/local/bin/act_runner register \
+    --no-interactive \
     --instance "${GITEA_INSTANCE_URL}" \
     --token "${GITEA_RUNNER_REGISTRATION_TOKEN}" \
-    --name "${GITEA_RUNNER_NAME:-railway-runner}";then
+    --name "${GITEA_RUNNER_NAME}"; then
     echo "❌ Registration FAILED! Check token/URL in logs."
     exit 1
   fi
@@ -13,4 +15,5 @@ if [ ! -f .runner ]; then
 else
   echo "✓ .runner exists—skipping."
 fi
+
 exec /usr/local/bin/act_runner daemon
