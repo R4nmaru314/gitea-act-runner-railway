e626aafff669fc78faa7aedd3966878e3689c7dc
r4nmaru <n.lehmann@r4nmaru.de>
2025-11-05 11:48:56 +0100

    prepare runner

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM gitea/act_runner:0.2.13
 
-RUN apk add --no-cache curl
-
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
-# Set entrypoint to run the test + daemon
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-
-echo "Runner config: NAME=${GITEA_RUNNER_NAME}, LABELS=${GITEA_RUNNER_LABELS}, EPHEMERAL=${GITEA_RUNNER_EPHEMERAL:-0}"
-
-# Start the actual runner daemon
 exec /usr/local/bin/act_runner daemon
